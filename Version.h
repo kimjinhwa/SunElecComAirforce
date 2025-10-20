0a59123dec7dea8445b4ed17527300f36fb7c9e9
mvcjhkim <mvchkim@gmail.com>
2025-10-20 13:36:22 +0900

    새로운 프토토콜 즉 나라다 자체 프로토콜을 시작한다.

--- Version.h
+++ Version.h
@@ -1,4 +1,5 @@
-#define version "VER_1.1.2"  // Add Module Count to Modbus Reader from database
+#define version "VER_2.0.0"  // 새로운 프토토콜 즉 나라다 자체 프로토콜을 시작한다. 
+//#define version "VER_1.1.2"  // Add Module Count to Modbus Reader from database
 //#define version "VER_1.1.1"  // Change Logger to Winston
 //#define version "VER_1.1.0"  // Add Discharge Log Table and Alarm Log Table
 //#define version "VER_1.0.1"  // Web Api api/battery is now working 
